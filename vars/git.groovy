2f6ec5fb3d62d40b0f19100f8a4462842b94c6a0
Amanpurohit <92878588+Amanpurohit203@users.noreply.github.com>
2026-01-30 13:49:25 +0530

    Parameterize git clone URL and branch

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,5 +1,5 @@
 def call(string url, string branch){
    echo "cloning the code"
-                git branch: 'main', url: 'https://github.com/Amanpurohit203/django-notes-app.git'
-                echo "cloned succesfully"
+   git branch: 'branch', url: 'url'
+   echo "cloned succesfully"
 }
